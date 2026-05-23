0c902061e9e8ccc016e48292fc3fdee92f2a4e17
Isaias-05 <151371346+Isaias-05@users.noreply.github.com>
2026-05-23 17:44:31 -0300

    2

--- mensajeria.py
+++ mensajeria.py
@@ -1,13 +1,34 @@
 import threading
-
+import socket
+import sys
 
 def receptor():
     while True:
+
+
+
         pass
 
 def emisor():
     while True:
+
+    
+
         pass
+
+# Inicio "main"
+print("Mensajeria Redes 2025")
+
+
+# Validamos argumentos
+if len(sys.argv) < 3:
+	print(" Error: faltan argumentos. Uso: cliente.py ipDest puertoDest")
+	sys.exit(1)
+
+
+
+
+
 
 tReceptor = threading.Thread(target=receptor, daemon=True)
 tEmisor = threading.Thread(target=emisor)
